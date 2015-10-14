e7c3c2d21597f8eb8e1855cd3ef7c35a30a55dc0
Anna Tataranovich <steshan86@gmail.com>
2015-10-14 21:17:07 +0300

    Time conversion moved to separate function. Argument support added.

--- src/program.py
+++ src/program.py
@@ -4,14 +4,25 @@ import datetime
 import sys
 import zipfile
 import xml.etree.ElementTree as et
+import argparse
 
-
-#??????big-little ending
 
 reload(sys)
 sys.setdefaultencoding('utf8')
 
-filename = "/home/ann/Development/jtv2xmltv-master/examples/TelecomTVepg.zip"
+def filetime_to_datetime (time):
+    filetime = struct.unpack("<Q", time)
+    timestamp = filetime[0]/10
+    return datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=timestamp);
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-i', '--inputfile', required = True)
+parser.add_argument('-o', '--outputfile', default='-')
+parser.add_argument('-t', '--timezone')
+args = parser.parse_args()
+
+filename = args.inputfile
 archive = zipfile.ZipFile(filename, 'r')
 idtv=0
 top = et.Element('tv')
@@ -32,32 +43,17 @@ for filename in archive.namelist():
         pdt_data = pdtfile.read()
         if pdt_data[0:26]!= "JTV 3.x TV Program Data\x0A\x0A\x0A":
             raise BaseException("incorrect file")
-        #pdtfile.close()
         ndxfile = archive.open(ndxfile_name, 'r')
         ndx_data=ndxfile.read()
         recnum = ndx_data[0:2]
         ndx_data = ndx_data[2:]
-        recordnum = struct.unpack("<H", recnum)
-        for i in range(0, recordnum[0]):
-
-            time = ndx_data[2:10]
-            #todo вынести преобразование времени в функцию
-            filetime = struct.unpack("<Q", time)
-            timestamp = filetime[0]/10
-            starttime = datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=timestamp)
-
-            #print recordnum[0]
-            if i<> recordnum[0]-1:
+        recordnum = struct.unpack("<H", recnum)[0]
+        for i in range(0, recordnum):
+            timestart = ndx_data[2:10]
+            starttime = filetime_to_datetime(timestart)
+            if i<> recordnum-1:
                 timestop = ndx_data[14:22]
-                #print i
-                #print len(timestop)
-                #todo вынести преобразование времени в функцию
-                filetimestop = struct.unpack("<Q", timestop)
-                timestampstop = filetimestop[0]/10
-                stoptime = datetime.datetime(1601, 1, 1) + datetime.timedelta(microseconds=timestampstop)
-                #print stoptime
-
-
+                stoptime = filetime_to_datetime(timestop)
             pdt = ndx_data[10:12]
             pdtindex = struct.unpack("<H", pdt)
             pdt_title_offset=pdtindex[0]
@@ -65,12 +61,19 @@ for filename in archive.namelist():
             Amount = struct.unpack("<H", Amt)
             name = pdt_data[pdt_title_offset+2:pdt_title_offset+2+Amount[0]]
             unicode_name = name.decode('cp1251').encode('UTF-8')
-            # TODO добавить возможность указать timezone
-            programme = et.SubElement(top, 'programme', start=starttime.strftime('%Y%m%d%H%M%S +0300'), stop=stoptime.strftime('%Y%m%d%H%M%S +0300'), channel=str(idtv))
+            if args.timezone is None:
+                tz_format = '%Y%m%d%H%M%S'
+            elif args.timezone[0]=='-' or args.timezone[0]=='+':
+                tz_format = '%Y%m%d%H%M%S ' + args.timezone
+            else:
+                tz_format = '%Y%m%d%H%M%S +' + args.timezone
+            programme = et.SubElement(top, 'programme', start=starttime.strftime(tz_format), stop=stoptime.strftime(tz_format), channel=str(idtv))
             title = et.SubElement(programme, 'title')
             title.text = unicode_name
             ndx_data = ndx_data[12:]
 archive.close()
-#print et.tostring(top, encoding='utf8')
 xmltv=et.ElementTree(element=top)
-xmltv.write('test.xml', encoding="utf8", xml_declaration=True)
+if args.outputfile !='-':
+    xmltv.write(args.outputfile, encoding="utf8", xml_declaration=True)
+else:
+    print et.tostring(top, encoding='utf8')
