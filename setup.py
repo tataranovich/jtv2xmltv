7d568d36089df12fac65fc727905619fb0dcedcc
Andrey Tataranovich <tataranovich@gmail.com>
2019-05-01 18:07:28 +0300

    Cleanup setup

--- setup.py
+++ setup.py
@@ -3,22 +3,18 @@ from setuptools import setup, find_packages
 
 setup(
     name="jtv2xmltv",
-    version="0.1.0",
+    version="0.1.1",
     packages=find_packages(),
-
     entry_points={
         'console_scripts': [
             'jtv2xmltv = jtv2xmltv.main:main',
         ]
     },
-
-    install_requires=[],
-
     author="Andrey Tataranovich",
     author_email="tataranovich@gmail.com",
     description="JTV to XMLTV converter",
+    long_description="JTV to XMLTV converter",
     license="GPL-3",
     keywords="epg xmltv jtv",
     url="https://github.com/tataranovich/jtv2xmltv/",
-
 )
